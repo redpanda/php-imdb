b2ca7ee8291c5d4b6ed785b71f229c4ac22509f9
Jimmy Leger <jimmy.leger@gmail.com>
2012-04-20 10:45:37 +0200

    add SearchTest::testSearchWithExactMatch() for testing exact match

--- tests/IMDb/Tests/SearchTest.php
+++ tests/IMDb/Tests/SearchTest.php
@@ -3,6 +3,7 @@
 namespace IMDb\Tests;
 
 use IMDb\Search;
+use IMDb\Movie;
 
 class SearchTest extends \PHPUnit_Framework_TestCase
 {
@@ -23,6 +24,16 @@ class SearchTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertTrue(is_bool($this->search->exactMatch()));
         $this->assertFalse($this->search->exactMatch());
+    }
+
+    public function testSearchWithExactMatch()
+    {
+        $search = new Search('I killed my lesbian wife');
+        $movies = $search->getMovies();
+
+        $this->assertTrue(is_array($movies));
+        $this->assertSame(1, count($movies));
+        $this->assertSame('I Killed My Lesbian Wife, Hung Her on a Meat Hook, and Now I Have a Three-Picture Deal at Disney', $movies[0]->getTitle());
     }
 
     public function tearDown()
